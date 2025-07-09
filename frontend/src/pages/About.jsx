08e8db29d819ae7611bf24c09e2f95c7499642fd
snehasingh022 <snehasingh842003@gmail.com>
2025-07-09 15:12:16 +0530

    fix: corrected casing mismatch for NewsletterBox

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
-import NewsletterBox from '../components/NewsletterBox'
+import NewsletterBox from '../components/NewsLetterBox'
 import { Link } from 'react-router-dom'
 
 const About = () => {
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
-import NewsLetterBox from '../components/NewsletterBox'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 
 
