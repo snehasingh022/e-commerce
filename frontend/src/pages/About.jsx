4a47ca06362bd183750bea07e311e81ce2135b2c
snehasingh022 <163733227+snehasingh022@users.noreply.github.com>
2025-07-09 15:36:19 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
-import NewsLetterBox from '../components/NewsLetterBox'
+import NewsletterBox from '../components/NewsLetterBox'
 import { Link } from 'react-router-dom'
 
 const About = () => {
